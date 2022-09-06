8725afe0d7ebdf43fc659217deef8830925c90f9
黄铖 <11490874+h_cheng@user.noreply.gitee.com>
2022-09-06 16:13:13 +0800

    feat:postcss配置完成

---
+++ .postcssrc.js
@@ -0,0 +1,17 @@
+// module.exports = {
+//   plugins: {
+//     'postcss-pxtorem': {
+//       rootValue: 37.5,
+//       propList: ['*']
+//     }
+//   }
+// }
+const pxToRem = require('postcss-pxtorem')
+module.exports = {
+  plugins: [
+    pxToRem({
+      rootValue: ({ file }) => (/vant/.test(file) ? 37.5 : 75),
+      propList: ['*']
+    })
+  ]
+}
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>黑马头条</div>
+  <div>
+    黑马头条
+    <span class="toutiao toutiao-gengduo"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <div class="axs">动起来了</div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+.axs {
+  width: 1rem;
+  height: 1rem;
+  background-color: lawngreen;
+}
+</style>
